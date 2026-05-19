3dd400ec2d1d86d23459c7bfc6e44b32df3d3e33
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2026-05-19 10:52:36 +0500

    jjjjjkkjkjkj877878

--- frontend/src/pages/Message.jsx
+++ frontend/src/pages/Message.jsx
@@ -6,7 +6,7 @@ const Message = ({ msg }) => {
     document.querySelector('.message').style.display = 'none';
   }, 5000);
   return (          
-        <div className={`${msg.length  <20 ? 'h-[100px]' : 'h-[150px]'} message z-40 flex items-center w-[80%] fixed top-[4rem] left-1/2 -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded`} >
+        <div className={`${msg.length  <30 ? 'h-[100px]' : 'h-[150px]'} message z-40 flex items-center w-[80%] fixed top-[4rem] left-1/2 -translate-x-1/2 bg-red-500 text-white px-4 py-2 rounded`} >
           <h1 className='text-lg font-bold'>{msg}</h1>
         </div>
   )
